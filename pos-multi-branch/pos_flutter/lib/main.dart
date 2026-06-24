43fe0a509f0e49c862306b265322738296670d09
fawaid1892 <fawaid1892@users.noreply.github.com>
2026-06-24 18:36:10 +0000

    sprint2: frontend POS screen + barcode scan + checkout

---
+++ pos-multi-branch/pos_flutter/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/auth_provider.dart';
+import 'providers/cart_provider.dart';
+import 'screens/login_screen.dart';
+import 'screens/pos_screen.dart';
+import 'screens/checkout_screen.dart';
+import 'screens/receipt_screen.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => CartProvider()),
+      ],
+      child: const PosApp(),
+    ),
+  );
+}
+
+class PosApp extends StatelessWidget {
+  const PosApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'POS Multi Branch',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: const Color(0xFF1565C0),
+        useMaterial3: true,
+        brightness: Brightness.light,
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          elevation: 1,
+        ),
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/pos': (context) => const PosScreen(),
+        '/checkout': (context) => const CheckoutScreen(),
+        '/receipt': (context) => const ReceiptScreen(),
+      },
+    );
+  }
+}
